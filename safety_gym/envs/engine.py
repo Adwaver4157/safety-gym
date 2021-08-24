8241762b875f5d54e32887503b636df9f00af4c9
adwaver <adwaver4157@gmail.com>
2021-08-24 16:29:32 +0900

    fix: change point_straight action[0] #1

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -1514,8 +1514,8 @@ class Engine(gym.Env, gym.utils.EzPickle):
             action = np.array(action, copy=False)
             # forward movement range [0.01, 0.03]
             # z-axis rotation range [-0.25, 0.25]
-            # action[0] = 0.02 * (1 + action[0])
-            action[0] = np.abs(action[0])
+            action[0] = 0.1 * (1 + action[0])
+            # action[0] = np.abs(action[0])
             # action[1] /= 2.
 
         # Set action
